4b4d1bc7c9e222ec6cfdfe0421fa2d6fddf6f749
Brian David Lehto <bdlden1978@outlook.com>
2018-01-07 23:17:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign(playlist, {[artistName]: songTitle})
   return playlist
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete.playlist.artistName
+  return playlist
+} 
